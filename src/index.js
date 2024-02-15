f584603420fd5f1edaad9822b411e426468a2ad8
Parth Khatri <p.khatri21@gmail.com>
2024-02-15 18:43:40 +1100

    Simplified

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+// import bodyParser from 'body-parser';
 
-import models from './models';
 import routes from './routes';
 
 const app = express();
@@ -18,21 +18,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Custom Middleware
-
-app.use((req, res, next) => {
-  req.context = {
-    models,
-    me: models.users[1],
-  };
-  next();
-});
-
 // * Routes * //
 
-app.use('/session', routes.session);
-app.use('/users', routes.user);
-app.use('/messages', routes.message);
+app.use('/routes', routes);
 
 // * Start * //
 
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+  
+const routes = Router();
+
+routes.post('/', (req, res) => {
+  
+});
+
+export default routes;
